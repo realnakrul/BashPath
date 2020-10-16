9f6d84137c0d0456fb1f63db354e97871ad4164f
realnakrul <realnakrul@gmail.com>
2020-10-16 15:21:10 +0400

    Exercise 7 completed

---
+++ Exercise07/tshoot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Troubleshoot the script
+# Define the network that is tested
+
+echo On what network do you want to test? \(Please enter a 4 byte number\)
+echo Press enter if you want to scan the local network
+read NETWORK
+[ -z $NETWORK ] && NETWORK=`ip a s | grep ens33 | head -n 3 | tail -n 1 | awk '{print $2 }'` && NETWORK=${NETWORK%/}
+NETWORK=${NETWORK%.*}
+
+# Remove old uplist file if existing
+rm /tmp/uplist
+
+# Run the program
+for (( NODE=1; NODE<20; NODE++ )); do
+	if ping -c 1 $NETWORK.$NODE
+	then
+	echo $NETWORK.$NODE >> /tmp/uplist
+	fi
+done
+
+echo press enter to display the list
+read
+
+cat /tmp/uplist
+exit 0
